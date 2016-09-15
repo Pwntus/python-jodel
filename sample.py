c88b2926769c4886147a091a545f670d7f35dbaf
Pwntus <pontus.edvard@gmail.com>
2016-09-16 01:33:39 +0200

    Updated sample program

--- sample.py
+++ sample.py
@@ -2,3 +2,18 @@ import python_jodel
 
 client = python_jodel.Client()
 
+# Get client karma
+response = client.get_karma()
+if response[0] != 200:
+	print('Something went wrong while fetching karma')
+else:
+	print('Client karma: %d\n\n' % response[1]['karma'])
+
+# Fetch 10 most recent posts
+response = client.get_posts_recent(limit = 10)
+
+if response[0] != 200:
+	print('Something went wrong while fetching recent posts')
+else:
+	for post in response[1]['posts']:
+		print('[Date: %s, ID: %s, vote count: %d]\n%s\n\n' % (post['created_at'], post['post_id'], post['vote_count'], post['message']))
